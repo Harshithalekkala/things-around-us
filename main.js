ed931048463bb97611c6f805e79b2ed7a76e3a61
Harshithalekkala <80406614+Harshithalekkala@users.noreply.github.com>
2022-02-26 17:49:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function draw() {
   image(video,0,0,300,300);
   classifier.classify(video,gotResult);
 }
-
+var previous_result="";
 function gotResult(error,results) {
   if (error) {
     console.error(error);
